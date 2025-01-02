732ed4d39fbc643bec5840a9d3612203b104685c
Sansan1980 <152459976+Sansan1980@users.noreply.github.com>
2025-01-02 16:37:45 +0300

    Initial commit

---
+++ greeter030125/src/main/java/com/skypro/greeter030125/Greeter030125Application.java
@@ -0,0 +1,13 @@
+package com.skypro.greeter030125;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Greeter030125Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Greeter030125Application.class, args);
+	}
+
+}
